ce297627543a5a0105ff0eab95b0b8bf48b431c3
Nedeljko Babic <bnedeljko0@gmail.com>
2023-07-20 12:52:51 +0200

    retrofit set

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "95c68beb3aac45148fea0fef5b1066f5"
+
+        const val BASE_URL = "https://newsapi.org"
+
+    }
+}
