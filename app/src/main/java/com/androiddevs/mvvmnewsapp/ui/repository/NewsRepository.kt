52c6df70a731809e6a8f931ad09c0b8effa8051e
Nedeljko Babic <bnedeljko0@gmail.com>
2023-07-21 18:15:39 +0200

    breaking news can be displayed now

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
@@ -0,0 +1,13 @@
+package com.androiddevs.mvvmnewsapp.ui.repository
+
+import com.androiddevs.mvvmnewsapp.ui.api.RetrofitInstance
+import com.androiddevs.mvvmnewsapp.ui.database.articleDB
+
+class NewsRepository(
+    val db: articleDB
+
+) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode,pageNumber)
+}
